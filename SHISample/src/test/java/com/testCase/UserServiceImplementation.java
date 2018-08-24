6f7b48567b2b0072d9c1ce90df22a2a616a83f91
nperugup <nperugup@NPERUGUP-0FCAE.partnet.cisco.com>
2018-08-24 15:31:28 +0530

    Mock

--- SHISample/src/test/java/com/testCase/UserServiceImplementation.java
+++ SHISample/src/test/java/com/testCase/UserServiceImplementation.java
@@ -6,9 +6,11 @@ import static org.mockito.Mockito.when;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import com.boraji.tutorial.spring.dao.UserDao;
 import com.boraji.tutorial.spring.entity.User;
@@ -16,17 +18,33 @@ import com.boraji.tutorial.spring.service.UserServiceImp;
 
 public class UserServiceImplementation {
 
-@Mock
-UserDao userdao;
+	@Mock
+	UserDao userdao;
 	
-@InjectMocks
-UserServiceImp userSercice;
+	@InjectMocks
+	UserServiceImp userSercice;
+	
+
+	   @Before
+	    public void init(){
+	        MockitoAnnotations.initMocks(this);
+	}
+	
 
 @Test
 public void userServiceImplementtest() {
 		
-User user1=new User("Lokendra","Gupta","CISCO");
-User user2=new User("Loks","Gup","HP");
+User user1=new User();
+user1.setId(1);
+user1.setEmail("lgupta065@gmail.com");
+user1.setFirstName("Lokendra");
+user1.setLastName("Gupta");
+User user2=new User();
+user2.setId(2);
+user2.setEmail("lgupta065@gmail.com");
+user2.setFirstName("Lokendra");
+user2.setLastName("Gupta");
+
 List<User> user=new ArrayList<User>();
 user.add(user1);
 user.add(user2);
